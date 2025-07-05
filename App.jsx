1cba0176fa1b9431953f7cad37b21839673bb70a
jkuch89 <jkuch89@gmail.com>
2025-07-05 19:29:38 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+      <h1>Welcome to My Friend CRM</h1>
+      <p>This is your live React frontend.</p>
+    </div>
+  );
+}
+
+export default App;
